b3d6fed267a52c72cda7d04ce804b0bf28e77318
Dibya Dahal <102875464+Killerbee7@users.noreply.github.com>
2022-10-26 14:13:34 +0300

    Counter Ready

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,26 +1,64 @@
 import React, { Component } from "react";
+import Button from "./Button";
+
 
 class Main extends Component {
   state = { counter: 100, };
 
+
   addOneHandler = () =>{
     this.setState({counter: this.state.counter + 1})
+  };
 
+  addFiveHandler = () => {
+    this.setState({counter: this.state.counter + 5})
+  
+  };
+
+  addResetHandler = () => {
+    this.setState({counter: this.state.counter=100});
+  };
+
+  removeOneHandler = () => {
+    if(this.state.counter >= 1){
+    this.setState({counter: this.state.counter -1 });
+  }
+  else{
+    this.setState({counter: this.state.counter = 0 })
+  }
+  
+  };
+  removeFiveHandler = () => {
+    if(this.state.counter >= 5){
+      this.setState({counter: this.state.counter -5 })
+    }
+    else{
+      this.setState({counter: this.state.counter = 0 })
+    }
+    
   };
   render() {
+    let counterClass= "";
+    this.state.counter===0 || this.state.counter===100
+    ?(counterClass= "counter")
+    : this.state.counter%2===0
+    ?(counterClass= "even")
+    
+    :(counterClass= "odd")
+  
     return (
       <main>
         <div className="container">
-          <div className="counter">
+          <div className={counterClass}>
             <p>{this.state.counter}</p>
           </div>
 
           <div className="button-20">
-            <button>Add Five</button>
-            <button onClick={this.addOneHandler}>Add One</button>
-            <button>Reset</button>
-            <button>Remove One</button>
-            <button>Remove Five</button>
+            <Button click={this.addFiveHandler}>Add Five</Button>
+            <Button click={this.addOneHandler}>Add One</Button>
+            <Button click={this.addResetHandler}>Reset</Button>
+            <Button click={this.removeOneHandler}>Remove One</Button>
+            <Button click={this.removeFiveHandler}>Remove Five</Button>
           </div>
         </div>
       </main>
